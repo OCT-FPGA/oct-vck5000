17a253ca2e61f349becabaef5b295a0f65a8bf62
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-02-19 22:22:24 -0500

    Update post-boot-cuda.sh

--- post-boot-cuda.sh
+++ post-boot-cuda.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install -y nvidia-driver-535
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt update
-sudo apt --fix-broken install -y
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y cuda-12-2
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+#sudo dpkg -i cuda-keyring_1.1-1_all.deb
+#sudo apt update
+#sudo apt --fix-broken install -y
+#sudo apt update && sudo apt upgrade -y
+#sudo apt install -y cuda-12-2
