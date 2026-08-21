11d61a6188a3c4770ab3d5de9f3de43d251c004e
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2026-08-21 09:39:55 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -22,8 +22,8 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Pick your image.
-imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04'),
-             ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04')] 
+imageList = [('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+             ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04')] 
 
 dockerImageList = [('pytorch')]
 workflow = ['Vitis', 'Vitis-AI']
